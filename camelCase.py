efed1503eb68c180cf7ecd06e409ae180a2918ab
Noelle <johndoe@example.com>
2022-08-25 21:29:01 -0500

    final commit for lab 1

--- camelCase.py
+++ camelCase.py
@@ -1,23 +1,25 @@
-'''Noelle this program turns a sentence into camelcase'''
+'''Noelle this program attempts to turn a sentence into camelcase, its not quite right'''
 
 #get a sentence to split 
 def main():
     sentence_to_split = input('Enter a sentence and it will be transformed: ')
-    conversion = sentence_to_split
-sentence_to_split.split()
-
-
+    conversion(sentence_to_split)
+    sentence_to_split.split()
 
 def conversion(sentence_to_split):
     word_list = sentence_to_split.split()
     new_list = []
 
     finished_string = ''
-
+    #for loop Nate helped with this in breakout rooms, Im still not sure about it.
     for index, word in enumerate(word_list):
         if index == 0:
             word = word.lower()
         else:
-            word = word.lower()
-            word_list.append(word)
-    return finished_string.join(word_list)
+            word = word.capitalize()
+            new_list.append(word)
+            # return the list of words
+    return finished_string.join(new_list)
+
+
+main()
